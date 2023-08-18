032c0fc5f094673085c648741d8410de2abc0177
Bradford, Glenn (gb1175) <gb1175@us.att.com>
2023-08-18 11:54:01 -0500

    Revised url for php's to include a localRoot value, hard coded the environment name.

--- a11y/main.js
+++ a11y/main.js
@@ -60,7 +60,8 @@ const MSG_TYPE_UPDATE = "msg_type_update";
 * Declare the environment variables
 * */
 
-environment = process.env.NODE_ENV
+//environment = process.env.NODE_ENV
+environment = 'local';
 
 /*
 * Generate the hostname based off the env
@@ -70,6 +71,15 @@ if (environment === 'local') hostname = "http://localhost:80/"
 else if (environment === 'development') hostname = "https://sp-compliance.dev.att.com/"
 else if (environment === 'production') hostname = "https://sp-compliance.web.att.com/"
 else throw new Error("the environment variable is not defined.");
+
+/*
+* Generate local root value to manage connection
+*/
+let localRoot = '/CTX_A11Y_Dash_Working/CTX_A11Y_Dash_Working/';
+
+if (environment === 'local') localRoot = '/CTX_A11Y_Dash_Working/CTX_A11Y_Dash_Working/'
+else if (environment === 'development') localRoot = ""
+else if (environment === 'production') localRoot = "";
 
 /*
 * STEP 1 - Get Urls
@@ -373,7 +383,7 @@ async function finalWrapup() {
 async function buildfinalScanReport(scanid = ctxScanApp.scanRecordId) {
 
     const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
-    const targetPg = hostname+'ax_dash_pg/cal/apps/aud/scanpost/6_buildFinalScanReport.php';
+    const targetPg = hostname+ localRoot + 'cal/apps/aud/scanpost/6_buildFinalScanReport.php';
 
     let scanLogMsg = {
         "scanid": scanid,
@@ -415,7 +425,7 @@ async function buildfinalScanReport(scanid = ctxScanApp.scanRecordId) {
 async function getUrlsToScan() {
 
     const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
-    const urlListLoc = hostname+'ax_dash_pg/cal/apps/aud/scanpost/1_getUrlToScan.php';
+    const urlListLoc = hostname+ localRoot + 'cal/apps/aud/scanpost/1_getUrlToScan.php';
 
     let status;
     let urlsRetrieved;
@@ -490,7 +500,7 @@ async function getUrlsToScan() {
 async function postScanRecord(msg, type, scanid = ctxScanApp.scanRecordId, scan_list_id = -1, timestamp = ctxScanApp.scanTimestamp) {
 
     const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
-    const targetPg = hostname+'ax_dash_pg/cal/apps/aud/scanpost/postScanRecord.php';
+    const targetPg = hostname+ localRoot + 'cal/apps/aud/scanpost/postScanRecord.php';
 
     let scanLogMsg = {
         "msg": msg,
@@ -541,7 +551,7 @@ async function postScanMetadata(scanid = ctxScanApp.scanRecordId, metaDataObj, s
     //console.log( "postScanMetadata(): scanListId =", scanListId );
 
     const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
-    const targetPg = hostname+'ax_dash_pg/cal/apps/aud/scanpost/2_addMetadataToScan.php';
+    const targetPg = hostname+ localRoot + 'cal/apps/aud/scanpost/2_addMetadataToScan.php';
 
     let metadataToAdd = {
         "scanMetadataAdded": ctxScanApp.currentScanSrc.scanMetadataPosted,
@@ -601,7 +611,7 @@ async function processStoredIssues() {
 
     //had to use this approach since this is not a module
     const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
-    const ctxIntakeUrl = hostname+'ax_dash_pg/cal/apps/aud/scanpost/4_processStoredIssues.php';
+    const ctxIntakeUrl = hostname+ localRoot + 'cal/apps/aud/scanpost/4_processStoredIssues.php';
 
     //Integrate relevant metadata
     let dataBlock = {
@@ -640,7 +650,7 @@ async function gradeNewIssues() {
 
     //had to use this approach since this is not a module
     const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
-    const gradingUrl = hostname+'ax_dash_pg/cal/apps/aud/scanpost/5_processGrades.php';
+    const gradingUrl = hostname+ localRoot + 'cal/apps/aud/scanpost/5_processGrades.php';
 
     //Integrate relevant metadata
     let dataBlock = {
@@ -690,7 +700,7 @@ async function sendAccessibilityConcernsToCTXAx(ampReportData) {
 
     //had to use this approach since this is not a module
     const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
-    const ctxIntakeUrl = hostname+'ax_dash_pg/cal/apps/aud/scanpost/3_postScanResults.php';
+    const ctxIntakeUrl = hostname+ localRoot + 'cal/apps/aud/scanpost/3_postScanResults.php';
 
     //Integrate relevant metadata
     let initialDataBlock = {
@@ -792,7 +802,7 @@ function getFullDateAndTime(timestamp = new Date(), forceTwoDigits = true, getSt
  */
 async function createScanReport() {
     const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
-    const targetPg = hostname+'ax_dash_pg/cal/apps/aud/scanpost/emailScanReport.php';
+    const targetPg = hostname+ localRoot + 'cal/apps/aud/scanpost/emailScanReport.php';
 
     await fetch(targetPg, {
         method: 'GET',
